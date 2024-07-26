b4d5fa0e684810b0e6fd9a712d8b6a5a56bd906e
huhangwei <huhangwei@gmail.com>
2024-07-26 17:34:29 +0800

    2.36.6

---
+++ AWSS3-WM/AWSS3-WM/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  AWSS3-WM
+//
+//  Created by Ricky on 2024/7/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AWSS3-WM/AWSS3-WM/DocumentViewController.h
@@ -0,0 +1,12 @@
+//
+//  DocumentViewController.h
+//  AWSS3-WM
+//
+//  Created by Ricky on 2024/7/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DocumentViewController : UIDocumentViewController
+
+@end
